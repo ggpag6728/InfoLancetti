870a0c81967c2664fe2a9f6af5421d5ac17a02b1
ggpag6728 <ggpag03s@gmail.com>
2026-05-16 00:27:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ st.markdown("""
 
 # --- BACKEND LOGIC (Invariato) ---
 MAPPA_LINEE_S = {
-    "VARESE": "S5", "TREVIGLIO": "S5", "CERTOSA" : "S5",
+    "VARESE": "S5", "TREVIGLIO": "S5",
     "NOVARA": "S6", "PIOLTELLO LIMITO": "S6",
     "LODI": "S1", "SARONNO": "S1", 
     "SEVESO": "S2", "MILANO ROGOREDO": "S2",
@@ -106,10 +106,10 @@ HEADERS_ATM = {
 }
 
 fermate_atm = [
-    FermataAtm(nome_identificativo="90 Jenner -> Lodi", poi_id="5641332"),
-    FermataAtm(nome_identificativo="91 Jenner -> Lotto", poi_id="5641333"),
     FermataAtm(nome_identificativo="92 Lancetti -> Bovisa", poi_id="5641319"),
-    FermataAtm(nome_identificativo="92 Lancetti -> Lodi", poi_id="5644379")
+    FermataAtm(nome_identificativo="92 Lancetti -> Lodi", poi_id="5644379"),
+    FermataAtm(nome_identificativo="90 Jenner -> Lodi", poi_id="5641332"),
+    FermataAtm(nome_identificativo="91 Jenner -> Lotto", poi_id="5641333")
 ]
 
 def get_treni(codice_stazione: str) -> List[Dict[str, Any]]:
@@ -124,7 +124,7 @@ def get_treni(codice_stazione: str) -> List[Dict[str, Any]]:
         i=0
         while(i<5 and len(treni_json)>=i):
             treno=treni_json[i]
-            stato = treno.get("arrivato", "false")
+            stato = treno.get("arrivato", False)
             if(stato!=True):
                 destinazione = treno.get("destinazione", "N/D")
                 binario = (treno.get("binarioEffettivoPartenzaDescrizione") or 
@@ -213,10 +213,10 @@ with requests_cffi.Session(impersonate="chrome110") as s:
     s.get("https://giromilano.atm.it/", headers=HEADERS_ATM)
     
     for fermata in fermate_atm:
-        st.markdown(f'<div style="color: #6b7280; font-weight: 600; margin: 15px 0 5px 5px; font-family: sans-serif; font-size: 14px;">{fermata.nome_identificativo}</div>', unsafe_allow_html=True)
         buses_data = get_atm(fermata, s)
         
         if buses_data:
+            bus_trovati = True
             for b in buses_data:
                 attesa = b['attesa'].lower()
                 if "in arrivo" in attesa:
@@ -225,20 +225,20 @@ with requests_cffi.Session(impersonate="chrome110") as s:
                     stato_css = "status-wait"
                 else:
                     stato_css = "status-neutral"
-                    
+                
                 riga_html = f"""
                 <div class="transport-row">
                     <span class="icon">directions_bus</span>
                     <div class="line-name">{b['linea']}</div>
-                    <div class="destination">{b['destinazione']}</div>
+                    <div class="destination">per {b['destinazione'].split('-')[-1].split('(')[0]}</div>
                     <div class="details">
                         <span class="{stato_css}">{b['attesa']}</span>
                     </div>
                 </div>
                 """
                 st.markdown(riga_html, unsafe_allow_html=True)
-        else:
-            st.markdown('<div style="color: #9ca3af; font-size: 14px; margin-left: 5px;">Nessun dato per questa fermata</div>', unsafe_allow_html=True)
+        if not bus_trovati:
+            st.markdown('<div style="color: #9ca3af; font-size: 14px; margin-left: 5px;">Nessun bus in arrivo</div>', unsafe_allow_html=True)
 
 # Logica di auto-aggiornamento ogni 60 secondi
 time.sleep(60)
